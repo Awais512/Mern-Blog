af12403fd7807ad23fa2ef30019064fd5086a13c
awais512 <raza.awais25@gmail.com>
2024-01-17 15:56:22 +0000

    Connecting to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connection is live...🚀"))
+  .catch((error) => console.log(`Something went wrong. ERROR: ${error}`));
 
 const app = express();
 
