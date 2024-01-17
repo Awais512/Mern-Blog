60d55a83ea0c4f83f61d49d5aaecbb1f65d14507
awais512 <raza.awais25@gmail.com>
2024-01-17 16:27:13 +0000

    Test routes and controllers

--- api/index.js
+++ api/index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+
+//Routes importing
+import userRoutes from "./routes/user.route.js";
+
 import dotenv from "dotenv";
 dotenv.config();
 
+// Initialize express
+const app = express();
+
+// Database Connections
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Database connection is live...🚀"))
   .catch((error) => console.log(`Something went wrong. ERROR: ${error}`));
 
-const app = express();
-
+// Node Server Setup
 app.listen(5000, () => console.log("Server is listening at prot 5000"));
+
+app.use("/api/user", userRoutes);
