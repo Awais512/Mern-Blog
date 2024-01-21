f10bb3b0b0d63a254183d2eb883688d870bf8ae1
awais512 <raza.awais25@gmail.com>
2024-01-21 17:29:21 +0000

    UI for profile

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,36 @@
+import { useSelector } from "react-redux";
+import { Button, TextInput } from "flowbite-react";
+
 const DashProfile = () => {
-  return <div>DashProfile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser.profilePicture}
+            alt="user"
+            className="rounded-full w-full h-full border-8 border-[lightgray] object-cover"
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput type="email" id="email" defaultValue={currentUser.email} />
+        <TextInput type="password" id="password" placeholder="Password" />
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 cursor-pointer flex justify-between mt-5">
+        <span>Delete Account</span>
+        <span>Signout</span>
+      </div>
+    </div>
+  );
 };
 
 export default DashProfile;
