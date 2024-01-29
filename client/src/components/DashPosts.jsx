01af66ac68a6a31437387e0f84df9453a8cf26b3
awais512 <raza.awais25@gmail.com>
2024-01-29 17:01:53 +0000

    Show More Posts

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,84 +1,58 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Table } from "flowbite-react";
-import { Link } from "react-router-dom";
+
+import PostTable from "./PostTable";
 
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-
-  console.log(userPosts);
+  const [showMore, setShowMore] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`/api/post/getposts?user=${currentUser._id}`);
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 8) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
-        console.log(error);
+        console.log(error.message);
       }
     };
-
     fetchPosts();
-  }, [currentUser._id]);
+  }, []);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 8) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
-          <Table>
-            <Table.Head>
-              <Table.HeadCell>Date Updated</Table.HeadCell>
-              <Table.HeadCell>Post Image</Table.HeadCell>
-              <Table.HeadCell>Post Title</Table.HeadCell>
-              <Table.HeadCell>Category</Table.HeadCell>
-              <Table.HeadCell>Delete</Table.HeadCell>
-              <Table.HeadCell>
-                <span>Edit</span>
-              </Table.HeadCell>
-            </Table.Head>
-            {userPosts.map((post) => (
-              <Table.Body className="divide-y" key={post._id}>
-                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <Table.Cell>
-                    {new Date(post.updatedAt).toLocaleDateString()}
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Link
-                      className="font-medium text-gray-900 dark:text-white"
-                      to={`/post/${post.slug}`}
-                    >
-                      <img
-                        src={post.image}
-                        className="w-20 h-10 object-cover bg-gray-500"
-                        alt={post.title}
-                      />
-                    </Link>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Link to={`/post/${post.slug}`}>{post.title}</Link>
-                  </Table.Cell>
-                  <Table.Cell>{post.category}</Table.Cell>
-                  <Table.Cell>
-                    <span className="font-medium bg-rose-500 text-white p-1.5 rounded-md hover:underline cursor-pointer">
-                      Delete
-                    </span>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Link
-                      className="bg-teal-500 dark:bg-[#a5f3fc] dark:text-black text-white p-1.5 rounded-md hover:underline cursor-pointer"
-                      to={`/update-post/${post._id}`}
-                    >
-                      <span>Edit</span>
-                    </Link>
-                  </Table.Cell>
-                </Table.Row>
-              </Table.Body>
-            ))}
-          </Table>
+          <PostTable
+            userPosts={userPosts}
+            showMore={showMore}
+            handleShowMore={handleShowMore}
+          />
         </>
       ) : (
         <p>You have no Posts</p>
---
+++ client/src/components/PostTable.jsx
@@ -0,0 +1,76 @@
+import PropTypes from "prop-types";
+import { Table } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const PostTable = ({ userPosts, showMore, handleShowMore }) => {
+  return (
+    <>
+      <Table>
+        <Table.Head>
+          <Table.HeadCell>Date Updated</Table.HeadCell>
+          <Table.HeadCell>Post Image</Table.HeadCell>
+          <Table.HeadCell>Post Title</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Delete</Table.HeadCell>
+          <Table.HeadCell>
+            <span>Edit</span>
+          </Table.HeadCell>
+        </Table.Head>
+        {userPosts.map((post) => (
+          <Table.Body className="divide-y" key={post._id}>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell>
+                {new Date(post.updatedAt).toLocaleDateString()}
+              </Table.Cell>
+              <Table.Cell>
+                <Link
+                  className="font-medium text-gray-900 dark:text-white"
+                  to={`/post/${post.slug}`}
+                >
+                  <img
+                    src={post.image}
+                    className="w-20 h-10 object-cover bg-gray-500"
+                    alt={post.title}
+                  />
+                </Link>
+              </Table.Cell>
+              <Table.Cell>
+                <Link to={`/post/${post.slug}`}>{post.title}</Link>
+              </Table.Cell>
+              <Table.Cell>{post.category}</Table.Cell>
+              <Table.Cell>
+                <span className="font-medium bg-rose-500 text-white p-1.5 rounded-md hover:underline cursor-pointer">
+                  Delete
+                </span>
+              </Table.Cell>
+              <Table.Cell>
+                <Link
+                  className="bg-teal-500 dark:bg-[#a5f3fc] dark:text-black text-white p-1.5 rounded-md hover:underline cursor-pointer"
+                  to={`/update-post/${post._id}`}
+                >
+                  <span>Edit</span>
+                </Link>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        ))}
+      </Table>
+      {showMore && (
+        <button
+          onClick={handleShowMore}
+          className="w-full text-teal-500 self-center text-sm py-7"
+        >
+          Show more
+        </button>
+      )}
+    </>
+  );
+};
+
+PostTable.propTypes = {
+  userPosts: PropTypes.array.isRequired,
+  showMore: PropTypes.bool.isRequired,
+  handleShowMore: PropTypes.func.isRequired,
+};
+
+export default PostTable;
