f53e1763da1b53223d03a4fd08d20e2dfb8543e0
awais512 <raza.awais25@gmail.com>
2024-01-31 23:50:59 +0000

    ScrollToTop Component

---
+++ client/src/components/CommentSection.jsx
@@ -0,0 +1,39 @@
+import Proptypes from "prop-types";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+function CommentSection({ postId }) {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="max-w-2xl mx-auto w-full p-3">
+      {currentUser ? (
+        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
+          <p>Signed in as:</p>
+          <img
+            src={currentUser.profilePicture}
+            alt={currentUser.username}
+            className="h-5 w-5 object-cover rounded-full"
+          />
+          <Link
+            to="/dashboard?tab=profile"
+            className="text-xs text-cyan-600 hover:underline"
+          >
+            @{currentUser.username}
+          </Link>
+        </div>
+      ) : (
+        <div>
+          You must be signed in to comment
+          <Link to="/sign-in">Sign in</Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+CommentSection.propTypes = {
+  postId: Proptypes.string.isRequired,
+};
+
+export default CommentSection;
