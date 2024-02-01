2303d207faeeb4252f7fd0338c9caf46268d89d0
awais512 <raza.awais25@gmail.com>
2024-02-01 02:37:10 +0000

    Comment System complete

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,8 +1,20 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  deleteComment,
+  editComment,
+  getComments,
+  getcomments,
+  likeComment,
+} from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
+router.get("/getComments/:postId", getComments);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
+router.get("/getcomments", verifyToken, getcomments);
 export default router;
