2790080af9f7784f5bf8fe99ac24ba2b3cbe673d
awais512 <raza.awais25@gmail.com>
2024-02-01 23:03:55 +0000

    Home Page

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function PostCard({ post }) {
   return (
-    <div className="group relative w-full border h-[400px] overflow-hidden rounded-lg sm:w-[430px] border-teal-500 hover:border-2 transition-all">
+    <div className="group relative w-full border h-[350px] overflow-hidden rounded-lg sm:w-[350px] border-teal-500 hover:border-2 transition-all">
       <Link to={`/post/${post.slug}`}>
         <img
           src={post.image}
